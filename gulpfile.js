419b61e8e1a796d56afd305ef3b72e2a2d9b615a
Aleksey Danilov <de.tempore@gmail.com>
2021-11-04 13:19:58 +0300

    First steps to make gulp-builder

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,21 @@
-function defaultTask(cb) {
-  // place code for your default task here
-  cb();
+const {src, dest, watch} = require('gulp');
+
+const g_stylus = require('gulp-stylus');
+const stylusSvgImport = require('stylus-svg');
+
+function renderStylus() {
+  return src('./#src/stylus/style.styl') 
+    .pipe(g_stylus({
+      use: [
+        stylusSvgImport()
+      ]
+    }))
+    .pipe(dest('./dist/css')) 
 }
 
-exports.default = defaultTask
+function toWatch(params) {
+  watch(['#src/stylus/**/*.styl'], renderStylus);
+}
+
+exports.renderStylus = renderStylus;
+exports.toWatch = toWatch;
